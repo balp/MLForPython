4c514b2848e8d34e65ac7d2eebc8caff2de38bea
Anders Arnholm <anders@arnholm.se>
2017-05-29 08:57:01 +0200

    Pt.36: Train with sklearn

--- titanic.py
+++ titanic.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn import preprocessing, cross_validation
+from sklearn import preprocessing
 import pandas as pd
 
 style.use('ggplot')
@@ -28,7 +28,7 @@ home.dest Home/Destination
 '''
 
 df = pd.read_excel('titanic.xls')
-print(df.head())
+#print(df.head())
 df.drop(['body', 'name'], 1, inplace=True)
 df.convert_objects(convert_numeric=True)
 df.fillna(0, inplace=True)
@@ -51,4 +51,26 @@ def handle_non_numerical_data(df):
     return df
 df = handle_non_numerical_data(df)
 
-print(df.head())
+df.drop(['boat'], 1, inplace=True)
+#print(df.head())
+
+X = np.array(df.drop(['survived'], 1).astype(float))
+X = preprocessing.scale(X)
+y = np.array(df['survived'])
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
+
+correct = 0
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    prediction = clf.predict(predict_me)
+    if prediction[0] == y[i]:
+        correct += 1
+
+correct_rate = correct / len(X)
+if correct_rate < 0.5:
+    correct_rate = 1 - correct_rate
+print(correct_rate * 100)
+
